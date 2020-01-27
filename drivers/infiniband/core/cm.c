1c6f0b74171df8609aec7f6375359a3f5cbf43dd
Jason Gunthorpe <jgg@mellanox.com>
2020-01-27 12:31:28 -0400

    RDMA/cm: Call ib_unregister_client before doing destruction
    This directly prevents new work from being queued.
    
    FIXME: add fixes line
    
    Change-Id: Ia0211634da69a461a1538829a1e0b6d371cacf2d
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/core/cm.c
+++ drivers/infiniband/core/cm.c
@@ -4472,12 +4472,13 @@ static void __exit ib_cm_cleanup(void)
 {
 	struct cm_timewait_info *timewait_info, *tmp;
 
+	ib_unregister_client(&cm_client);
+
 	spin_lock_irq(&cm.lock);
 	list_for_each_entry(timewait_info, &cm.timewait_list, list)
 		cancel_delayed_work(&timewait_info->work.work);
 	spin_unlock_irq(&cm.lock);
 
-	ib_unregister_client(&cm_client);
 	destroy_workqueue(cm.wq);
 
 	list_for_each_entry_safe(timewait_info, tmp, &cm.timewait_list, list) {
