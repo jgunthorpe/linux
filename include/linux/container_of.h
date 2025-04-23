e27b870eb3eaa21562d0b9261179bf46c78ca1bf
Jason Gunthorpe <jgg@nvidia.com>
2025-04-23 12:01:07 -0300

    container_of: Add alloc_container()
    It is quite common in Linux for subsystems to use a container_of()
    relationship between the subsystem struct and the driver struct:
    
      struct subsystem {...};
      struct driver {struct subsystem subsys; ...}
    
    A useful pattern to allocate and initialize this memory has the subsystem
    allocate the memory, initialize struct subsystem and then return it back
    to the driver for additional initialization:
    
      struct driver *drv = subsystem_alloc(sizeof(struct driver), ...)
      subsystem_put(&drv->subsys);
    
    Some cases might callback to the driver using a provided ops to initialize
    the struct driver.
    
    This is intended to provide a simpler and more consistent API for
    drivers. A driver never sees a partially initialized struct subsystem, and
    always uses subsystem_put(). If init/deinit ops are used then the driver
    has a cleanly paired setup/destroy function that is fully sequenced by the
    subsystem including for all error cases.
    
    To make this trivial the struct driver must place struct subsystem at the
    start of the struct so offsetof() == 0 and thus container_of() is simply a
    type case.
    
    Add an alloc_container() helper that documents this pattern and provides
    the required type checks:
    
       struct driver *drv;
       drv = alloc_container(struct driver, subsys, subsystem_alloc, ...);
    
    The macro checks that offsetof(struct driver, subsys) == 0 and that
    subsystem_alloc() returns the same type as &struct driver::subsys. It
    transparently flows through an ERR_PTR or NULL generated by the allocation
    function.
    
    The intention is for the subsystem to wrap this in its own helper, eg:
    
     #define vfio_alloc_device(dev_struct, member, dev, ops) \
    	alloc_container(struct dev_struct, member, _vfio_alloc_device, dev, ops)
    
    Suggested-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://lore.kernel.org/linux-rdma/67a697f02bc0b_2d1e2948a@dwillia2-xfh.jf.intel.com.notmuch/
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- include/linux/container_of.h
+++ include/linux/container_of.h
@@ -38,4 +38,52 @@
 		default: ((type *)container_of(ptr, type, member))	\
 	)
 
+/**
+ * alloc_container_sz - allocate a container struct that emebeds another struct
+ * @container_type: the type of the enclosing structure
+ * @container_sz: size to allocate
+ * @member: the structure member within container_type for embedded_type
+ * @alloc_fn: Allocation function to call
+ *
+ * This helper is useful allocate and initialize derived structs. Typically a
+ * subsystem will provide an allocate and initialize function for embedded_type.
+ * A user of the subsystem will provide the derived type container_type which
+ * encloses embedded_type and uses container_of() to move between the types.
+ *
+ * In this pattern the subsystem provides a function to both allocate and
+ * initialize embedded_type so that the subsystem normal destroy function can be
+ * used. This avoids exposing a failure path for allocated but uninitialized
+ * memory that complicates error unwind and prevents trivially using cleanup.h.
+ *
+ * To use this API the user is required to place embedded_type at the front of
+ * container_type.
+ *
+ * alloc_fn() accepts the size to allocate as the first argument and the VA_ARGS
+ * as the remainder. size is guaranteed by the macro to be at least
+ * sizeof(embedded_type).
+ *
+ * Returns a pointer of type container_type. Returns the same failure code as
+ * alloc_fn, either ERR_PTR or NULL.
+ */
+#define alloc_container_sz(container_type, container_sz, member, alloc_fn, \
+			   ...)                                            \
+	({                                                                 \
+		typeof_member(container_type, member) *res =               \
+			alloc_fn(container_sz, ##__VA_ARGS__);             \
+		static_assert(offsetof(container_type, member) == 0);      \
+		(container_type *)res;                                     \
+	})
+
+/**
+ * alloc_container - allocate a container struct that emebeds another struct
+ * @container_type: the type of the enclosing structure
+ * @member: the structure member within container_type for embedded_type
+ * @alloc_fn: Allocation function to call
+ *
+ * Helper to call alloc_container_sz() with sizeof(container_type).
+ */
+#define alloc_container(container_type, member, alloc_fn, ...)             \
+	alloc_container_sz(container_type, sizeof(container_type), member, \
+			   alloc_fn, ##__VA_ARGS__)
+
 #endif	/* _LINUX_CONTAINER_OF_H */
